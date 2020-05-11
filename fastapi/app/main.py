a49e2441d8284c188a90a6734293d002e459b78a
Koichi Miyamoto <m5813k@gmail.com>
2020-05-11 21:06:04 +0900

    Support multiple text
    labels.txtは例です

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,22 +1,48 @@
+from typing import List, Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 app = FastAPI()
 
 from . import req
 
+# ラベルデータはtensorflowのembedding_lookupなどを使えば良いかもしれないが、tensorflowに慣れてないので普通にファイルで渡す
+import os
+APP_DIR = os.path.abspath(os.path.dirname(__file__))
+labels = []
+with open(APP_DIR + '/labels.txt') as f:
+    labels = f.read().splitlines()
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World!"}
 
 class Item(BaseModel):
-    text: str
+    text: Union[str, List[str]]
 
 @app.post("/tokenize")
 def tokenize(item: Item):
-    instance = req.create_instance(item.text)
-    prediction = req.send_request([instance])
+    if(isinstance(item.text, list)):
+        result = text_to_result_list(item.text)
+    else:
+        result = text_to_result(item.text)
 
     return {
-            "text": item.text,
-            "prediction": prediction,
+            "result": result,
+            "labels": labels,
             }
+
+def text_to_result(text: str):
+    instances = [req.create_instance(text)]
+    prediction = req.send_request(instances)
+    return {
+            "text": text,
+            "prediction": prediction[0],
+            }
+
+# tensorflowに投げるときにまとめて投げたほうが速いのでmap的なことをせずに別関数として定義すつ
+def text_to_result_list(text: List[str]):
+    instances = [req.create_instance(t) for t in text] 
+    prediction = req.send_request(instances)
+    zipped = zip(text, prediction)
+    return [ {"text": t, "prediction": p } for (t, p) in zipped ]
